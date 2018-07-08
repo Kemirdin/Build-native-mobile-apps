b9adfcee7c47d9a0aa45dc85bbea4949704c5ae6
Kemirdin Kaynam <qemirdin@gmail.com>
2018-07-08 21:56:34 +0200

    Customize React Native components using Props

---
+++ tutorialProject/04-CustomizeReactNative/index.js
@@ -0,0 +1,6 @@
+import {AppRegistry} from 'react-native';
+import Robot from './components/Robot'
+import MultipleGreetings from './components/MultipleGreetings';
+// import App from './App';
+
+AppRegistry.registerComponent('tutorialproject', () => MultipleGreetings);
