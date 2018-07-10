4248358d098210ded28dc1f3a2801bbbd3815c7b
Kemirdin Kaynam <qemirdin@gmail.com>
2018-07-10 22:08:12 +0200

    State in a React Native Component

---
+++ tutorialProject/05-StateinReact/index.js
@@ -0,0 +1,4 @@
+import {AppRegistry} from 'react-native';
+// import App from './App';
+import TextBlink from './components/TextBlink';
+AppRegistry.registerComponent('tutorialproject', () => TextBlink);
