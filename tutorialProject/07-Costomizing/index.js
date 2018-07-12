349bd88cf2a7ed2c13e351e11bc94e6b6ef2e7b3
Kemirdin Kaynam <qemirdin@gmail.com>
2018-07-12 12:36:38 +0200

    Customize width and height props with Fixed and Flex dimensions

---
+++ tutorialProject/07-Costomizing/index.js
@@ -0,0 +1,6 @@
+import {AppRegistry} from 'react-native';
+// import App from './App';
+// import FixedDimension from './components/FixedDimension';
+import FlexDimension from './components/FlexDimension';
+
+AppRegistry.registerComponent('tutorialproject', () => FlexDimension);
