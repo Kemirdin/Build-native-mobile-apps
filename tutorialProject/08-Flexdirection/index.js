3e3961f6a798266c4f464482ec62a21c82f0fa0e
Kemirdin Kaynam <qemirdin@gmail.com>
2018-07-13 22:15:59 +0200

    Flex direction and justify content with Flexbox

---
+++ tutorialProject/08-Flexdirection/index.js
@@ -0,0 +1,6 @@
+import { AppRegistry } from 'react-native';
+// import App from './App';
+import FlexExample from './components/FlexExample'
+import JustifyContentExample from './components/JustifyContentExample';
+
+AppRegistry.registerComponent('tutorialproject', () => JustifyContentExample);
