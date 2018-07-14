01d917637d25a55afa557c41aa11feac2e8bf0f7
Kemirdin Kaynam <qemirdin@gmail.com>
2018-07-14 22:25:43 +0200

    Flexbox layout with align items

---
+++ tutorialProject/09-Flexboxlayout/components/JustifyContentExample.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+export default class JustifyContentExample extends Component {
+  render() {
+    return (
+        <View style={ {
+            backgroundColor: 'aquamarine',
+            // flex: 1,
+            height: 500,
+            flexDirection: 'column',
+            justifyContent: 'flex-start',
+            alignItems: 'stretch'
+            // alignItems: 'stretch'
+        } }>
+            <Text style={{width: 50, height: 50, backgroundColor: 'red'}} />
+            <Text style={{height: 50, backgroundColor: 'green'}} />
+            <Text style={{height: 50, backgroundColor: 'blue'}} />
+        </View>
+    );
+  }
+};
