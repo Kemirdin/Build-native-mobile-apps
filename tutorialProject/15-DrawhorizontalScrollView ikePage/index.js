342912985ec176710139b3e4990ba208d5b5d9cd
Kemirdin Kaynam <qemirdin@gmail.com>
2018-08-02 23:31:14 +0200

    ScrollView Draw a horizontal ScrollView like Page

---
+++ tutorialProject/15-DrawhorizontalScrollView ikePage/index.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+import { AppRegistry } from 'react-native';
+// import App from './App';
+import VerticalScrollView from './components/VerticalScrollView';
+import HorizontalScrollView from './components/HorizontalScrollView';
+
+AppRegistry.registerComponent('tutorialProject', () => HorizontalScrollView);
