f43f44d3f1f6c27cd61ca1a9d72979d62e1f5347
Kemirdin Kaynam <qemirdin@gmail.com>
2018-08-03 21:55:50 +0200

    Create ScrollView with ViewPagerAndroid in React Native

---
+++ tutorialProject/16-CreateScrollViewwithViewPagerAndroidinReactNative/index.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import { AppRegistry } from 'react-native';
+// import App from './App';
+import VerticalScrollView from './components/VerticalScrollView';
+//import HorizontalScrollView from './components/HorizontalScrollView';
+import ViewPagerExample from './components/ViewPagerExample';
+
+AppRegistry.registerComponent('tutorialProject', () => ViewPagerExample);
